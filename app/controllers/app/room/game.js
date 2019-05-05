16afb075f7e9adc6b94f334b200f5dbb8f983092
Fabián Moreno Islas <fabianislas@apps-fa.com>
2019-05-05 00:52:08 -0500

    2023, proxima pelicula de Avenger "The New Avengers"

--- app/controllers/app/room/game.js
+++ app/controllers/app/room/game.js
@@ -10,5 +10,15 @@ export default class AppRoomGameController extends Controller {
   @tracked user_username = this.user_global.getCookie("username");
   @tracked user_id = this.user_global.getCookie("id");
   @tracked user_email = this.user_global.getCookie("email");
-  
+
+  @action chooseOption(game, play){
+    console.log(game, play);
+
+    this.store.findRecord('play', play).then(function(play) {
+      // ...after the record has loaded
+      play.set('choice', $("#txtOption").val());
+      play.save();
+    });
+
+  }
 }
--- app/models/play.js
+++ app/models/play.js
@@ -8,4 +8,32 @@ export default class PlayModel extends Model {
   // @attr('number') game_id;
   @attr('number') choice;
 
+  get getChoice(){
+    switch (this.choice){
+      case 1:
+        return "Rock";
+      break;
+
+      case 2:
+        return "Paper";
+      break;
+
+      case 3:
+        return "Scissors";
+      break;
+
+      case 4:
+        return "Lizard";
+      break;
+
+      case 5:
+        return "Spock";
+      break;
+
+      default:
+        return "No choice";
+      break;
+    }
+  }
+
 }
